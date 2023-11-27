8279440542602d872320360a4452813312afda08
James Lako <74892097+Jameslako@users.noreply.github.com>
2023-11-27 14:18:05 -0500

    Update interface.rs with init, commit and clone methods

--- src/behavior_hiding/interface.rs
+++ src/behavior_hiding/interface.rs
@@ -1,72 +1,41 @@
-use std::io;
 
-pub struct BehaviorHidingModule {
-    user_interface: UserInterface,
-    api: Api,
-}
+pub mod interface {
+    use std::fs;
+    use std::path::Path;    
 
-impl BehaviorHidingModule {
-    pub fn new() -> Self {
-        Self {
-            user_interface: UserInterface::new(),
-            api: Api::new(),
-        }
+    //logic for creating the init directory
+    pub fn init(repo_root_path: &str) {
+    let repo_path = Path::new(repo_root_path).join(".mydvcs");
+    if !repo_path.exists() {
+        fs::create_dir_all(&repo_path).expect("Failed to create repository directory");
+        
+        println!("Initialized empty repository in {:?}", repo_path);
+    } else {
+        println!("Repository already exists at {:?}", repo_path);
     }
+}
+    //logic for teh clone method
+    pub fn clone(src_repo_path: &str, dst_repo_path: &str) {
+        // this assumes local system paths
+        let src_path = Path::new(src_repo_path).join(".mydvcs");
+        let dst_path = Path::new(dst_repo_path).join(".mydvcs");
+    
+        fs::create_dir_all(&dst_path).expect("Failed to create destination directory");
+        fs::copy(&src_path, &dst_path).expect("Failed to clone repository");
+        println!("Cloned repository from {:?} to {:?}", src_path, dst_path);
 
-    pub fn execute_command(&self, command: &str) {
-        if command.starts_with("cli:") {
-            let cli_command = &command[4..]; 
-            self.user_interface.handle_command(cli_command);
-        } else if command.starts_with("web:") {
-            let web_command = &command[4..]; 
-            self.api.handle_request(web_command);
+    }
+    
+    //logic for commit
+    pub fn commit(repo_root_path: &str, commit_message: &str) {
+        let repo_path = Path::new(repo_root_path).join(".mydvcs");
+        if repo_path.exists() {
+            //detecting and saving commit
+            println!("Committed changes: {}", commit_message);
         } else {
-            println!("Invalid command format.");
-        }
-    }
-}
-
-pub struct UserInterface;
-
-impl UserInterface {
-    pub fn new() -> Self {
-        Self
-    }
-
-    pub fn handle_command(&self, command: &str) {
-        match command {
-            "commit" => self.handle_commit(),
-            "help" => self.handle_help(),
-            _ => println!("Unknown command: {}", command),
+            println!("No repository found at {:?}", repo_path);
         }
     }
 
-    fn handle_commit(&self) {
-        println!("Committing changes...");
-    }
-
-    fn handle_help(&self) {
-        println!("Displaying help information...");
-    }
 }
 
-pub struct Api;
-
-impl Api {
-    pub fn new() -> Self {
-        Self
-    }
-
-    pub fn handle_request(&self, request: &str) {
-       
-        println!("Handling web request: {}", request);
-    }
-}
-
-fn main() {
-    let behavior_module = BehaviorHidingModule::new();
-
-    behavior_module.execute_command("cli:commit");
-    behavior_module.execute_command("cli:help");
-    behavior_module.execute_command("cli:list_files"); 
-}
