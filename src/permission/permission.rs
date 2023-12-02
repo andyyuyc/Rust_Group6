a9f1f011095c17dc366e312a977f4983ab9d1955
Silin Chen <143574213+Adrian0251014@users.noreply.github.com>
2023-12-02 14:47:59 -0500

    Update permission.rs

--- src/permission/permission.rs
+++ src/permission/permission.rs
@@ -1,6 +1,9 @@
 use std::io::{self, Write};
 use std::str::FromStr;
+use std::path::Path;
+use std::fs;
 
+// Define an enum for different types of permissions
 #[derive(Debug, PartialEq, Copy, Clone)]
 enum Permission {
     Read,
@@ -8,15 +11,25 @@ enum Permission {
     Execute,
 }
 
+// Function to verify permissions of a user on a given file path
 fn verify_permissions(user: &str, file_path: &str, permission: Permission) -> io::Result<()> {
-    // Simulate permission checking logic
+    // Check if the user string is empty
     if user.is_empty() {
         return Err(io::Error::new(io::ErrorKind::NotFound, "Invalid user"));
     }
+
+    // Check if the file path is empty, not in a valid format, or does not exist
     if file_path.is_empty() {
-        return Err(io::Error::new(io::ErrorKind::NotFound, "Invalid file path"));
+        return Err(io::Error::new(io::ErrorKind::InvalidInput, "Invalid file path: empty"));
     }
-    // Insufficient permission
+    if !Path::new(file_path).is_absolute() {
+        return Err(io::Error::new(io::ErrorKind::InvalidInput, "Invalid file path: path format error"));
+    }
+    if !fs::metadata(file_path).is_ok() {
+        return Err(io::Error::new(io::ErrorKind::NotFound, "Invalid file path: path does not exist"));
+    }
+
+    // Example logic for permission checking (can be replaced with real logic)
     if user == "valid_user" && file_path == "valid_path" && (permission == Permission::Read || permission == Permission::Execute) {
         return Err(io::Error::new(io::ErrorKind::PermissionDenied, "Insufficient permission"));
     }
@@ -24,6 +37,7 @@ fn verify_permissions(user: &str, file_path: &str, permission: Permission) -> io
     Ok(())
 }
 
+// Function to prompt the user for input
 fn prompt_input(prompt: &str) -> String {
     let mut input = String::new();
     print!("{}", prompt);
@@ -35,8 +49,9 @@ fn prompt_input(prompt: &str) -> String {
 fn main() {
     let user = prompt_input("Enter user: ");
     let file_path = prompt_input("Enter file path: ");
-
     let permission_input = prompt_input("Enter permission (Read, Write, Execute): ");
+
+    // Parse the permission input
     let permission = match Permission::from_str(&permission_input) {
         Ok(p) => p,
         Err(_) => {
@@ -45,10 +60,11 @@ fn main() {
         },
     };
 
+    // Verify permissions and print the result
     let result = verify_permissions(&user, &file_path, permission);
     match result {
-        Ok(()) => println!("Permission granted for {:?} on {:?} with {:?}", user, file_path, permission),
-        Err(e) => println!("Permission denied for {:?} on {:?} with {:?}: {}", user, file_path, permission, e),
+        Ok(()) => println!("Permission granted"),
+        Err(e) => println!("Permission denied"),
     }
 }
 
