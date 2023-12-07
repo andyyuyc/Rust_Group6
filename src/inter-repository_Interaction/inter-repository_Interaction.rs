f496a65ec032649ecc12b699182172e0a65b8d8b
Silin Chen <143574213+Adrian0251014@users.noreply.github.com>
2023-12-07 16:33:31 -0500

    Update inter-repository_Interaction.rs

--- src/inter-repository_Interaction/inter-repository_Interaction.rs
+++ src/inter-repository_Interaction/inter-repository_Interaction.rs
@@ -1,84 +1,112 @@
-use git2::{Repository, MergeOptions, FetchOptions, RemoteCallbacks, Cred, Error};
-use std::io;
+use std::fs::{self, File};
+use std::io::{self, BufRead, BufReader, Write, stdin, stdout};
 use std::path::Path;
 
-fn pull_changes(remote_repo_path: &str, local_branch: &str) -> io::Result<()> {
-    let repo = Repository::open(Path::new("."))?;
-    let mut remote = repo.find_remote(remote_repo_path)?;
 
-    let mut fo = FetchOptions::new();
-    let mut callbacks = RemoteCallbacks::new();
-    callbacks.credentials(|_url, username_from_url, _allowed_types| {
-        Cred::ssh_key(username_from_url.unwrap(), None, Path::new("~/.ssh/id_rsa"), None)
-    });
-    fo.remote_callbacks(callbacks);
+fn main() {
+    println!("pull/push");
+    let mut action = String::new();
+    stdin().read_line(&mut action).expect("Failed to read line");
 
-    remote.fetch(&[local_branch], Some(&mut fo), None)?;
+    match action.trim() {
+        "pull" => {
+            let (remote_path, local_path) = get_paths();
+            if let Err(e) = pull(&remote_path, &local_path) {
+                println!("Error during pull: {}", e);
+            }
+        },
+        "push" => {
+            let (local_path, remote_path) = get_paths();
+            if let Err(e) = push(&local_path, &remote_path) {
+                println!("Error during push: {}", e);
+            }
+        },
+        _ => println!("Invalid action. Please enter 'pull' or 'push'."),
+    }
+}
 
-    let fetch_head = repo.find_reference("FETCH_HEAD")?;
-    let fetch_commit = repo.reference_to_annotated_commit(&fetch_head)?;
 
-    let (analysis, _) = repo.merge_analysis(&[&fetch_commit])?;
+fn get_paths() -> (String, String) {
+    println!("Enter the source path:");
+    let mut source_path = String::new();
+    stdin().read_line(&mut source_path).expect("Failed to read source path");
 
-    if analysis.is_up_to_date() {
-        println!("Already up to date!");
-    } else if analysis.is_fast_forward() {
-        let refname = format!("refs/heads/{}", local_branch);
-        let mut reference = repo.find_reference(&refname)?;
-        reference.set_target(fetch_commit.id(), "Fast-Forward")?;
-        repo.set_head(&refname)?;
-        repo.checkout_head(None)?;
+    println!("Enter the destination path:");
+    let mut destination_path = String::new();
+    stdin().read_line(&mut destination_path).expect("Failed to read destination path");
+
+    (source_path.trim().to_string(), destination_path.trim().to_string())
+}
+
+
+fn detect_changes(file1: &str, file2: &str) -> io::Result<bool> {
+    let f1 = File::open(file1)?;
+    let f2 = File::open(file2)?;
+
+    let file1_lines = BufReader::new(f1).lines();
+    let file2_lines = BufReader::new(f2).lines();
+
+    let mut conflict = false;
+    for (line1, line2) in file1_lines.zip(file2_lines) {
+        match (line1, line2) {
+            (Ok(l1), Ok(l2)) => {
+                if l1 != l2 {
+                    println!("Conflict found:\nFile1: {}\nFile2: {}", l1, l2);
+                    conflict = true;
+                }
+            },
+            _ => break,
+        }
+    }
+
+    Ok(conflict)
+}
+
+
+fn synchronize_changes(source: &str, destination: &str) -> io::Result<()> {
+    let conflict = detect_changes(source, destination)?;
+
+    if conflict {
+        println!("Merge conflict detected. Manual resolution required.");
     } else {
-        println!("Non-fast-forward updates are not supported in this example.");
+        fs::copy(source, destination)?;
     }
 
     Ok(())
 }
 
-fn push_changes(local_repo_path: &str, remote_branch: &str) -> io::Result<()> {
-    let repo = Repository::open(Path::new(local_repo_path))?;
-    let mut remote = repo.find_remote("origin")?;
 
-    let mut push_options = git2::PushOptions::new();
-    let mut callbacks = RemoteCallbacks::new();
-    callbacks.credentials(|_url, username_from_url, _allowed_types| {
-        Cred::ssh_key(username_from_url.unwrap(), None, Path::new("~/.ssh/id_rsa"), None)
-    });
-    push_options.remote_callbacks(callbacks);
+fn pull(remote_path: &str, local_path: &str) -> io::Result<()> {
+    let remote_files = fs::read_dir(remote_path)?;
+    for entry in remote_files {
+        let entry = entry?;
+        let file_name = entry.file_name();
+        let source_file = Path::new(remote_path).join(&file_name);
+        let dest_file = Path::new(local_path).join(&file_name);
 
-    remote.push(&[&format!("refs/heads/{}:refs/heads/{}", local_repo_path, remote_branch)], Some(&mut push_options))?;
-
-    Ok(())
-}
-
-
-fn detect_changes(local_repo_path: &str, remote_repo_path: &str) -> Result<(), Error> {
-    let repo = Repository::open(Path::new(local_repo_path))?;
-    let remote = repo.find_remote(remote_repo_path)?;
-    repo.fetch(&[remote.name().unwrap()], None, None)?;
-
-    let local_head = repo.head()?.peel_to_commit()?;
-    let remote_head = repo.find_reference("FETCH_HEAD")?.peel_to_commit()?;
-
-    if local_head.id() != remote_head.id() {
-        println!("Changes detected.");
-    } else {
-        println!("No changes detected.");
+        if Path::exists(&dest_file) {
+            synchronize_changes(source_file.to_str().unwrap(), dest_file.to_str().unwrap())?;
+        } else {
+            fs::copy(source_file, dest_file)?;
+        }
     }
 
+    println!("Pull completed.");
     Ok(())
 }
 
-fn synchronize_changes(local_repo_path: &str, remote_repo_path: &str) -> Result<(), Error> {
-    let repo = Repository::open(Path::new(local_repo_path))?;
-    let mut remote = repo.find_remote(remote_repo_path)?;
 
-    remote.fetch(&["master"], None, None)?;
-    let fetch_head = repo.find_reference("FETCH_HEAD")?.peel_to_commit()?;
-    repo.set_head_detached(fetch_head.id())?;
-    repo.checkout_head(Some(git2::build::CheckoutBuilder::new().force()))?;
+fn push(local_path: &str, remote_path: &str) -> io::Result<()> {
+    let local_files = fs::read_dir(local_path)?;
+    for entry in local_files {
+        let entry = entry?;
+        let file_name = entry.file_name();
+        let source_file = Path::new(local_path).join(&file_name);
+        let dest_file = Path::new(remote_path).join(&file_name);
 
-    remote.push(&["refs/heads/master:refs/heads/master"], None)?;
+        fs::copy(source_file, dest_file)?;
+    }
 
+    println!("Push completed.");
     Ok(())
 }
