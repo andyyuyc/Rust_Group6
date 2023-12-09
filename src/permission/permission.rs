0ec1d3433fd06b7d9c90155606e6a7c43db284e7
Silin Chen <143574213+Adrian0251014@users.noreply.github.com>
2023-12-09 18:50:14 -0500

    Update permission.rs

--- src/permission/permission.rs
+++ src/permission/permission.rs
@@ -78,3 +78,76 @@ impl FromStr for Permission {
         }
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    // Test for an invalid user and a valid file path with the write permission
+    #[test]
+    fn test_invalid_user_valid_path_write_permission() {
+        let user = "";
+        let file_path = "/Users/adrian/Desktop"; 
+        let permission = Permission::Write;
+
+        let result = verify_permissions(user, file_path, permission);
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err().kind(), io::ErrorKind::NotFound);
+    }
+
+    // Test for a valid user and an invalid file path with the execute permission
+    #[test]
+    fn test_valid_user_invalid_path_execute_permission() {
+        let user = "valid_user";
+        let file_path = "";
+        let permission = Permission::Execute;
+
+        let result = verify_permissions(user, file_path, permission);
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err().kind(), io::ErrorKind::InvalidInput);
+    }
+
+    // Test for a valid user, a valid file path, and an invalid permission
+    #[test]
+    fn test_valid_user_valid_path_invalid_permission() {
+        let user = "valid_user";
+        let file_path = "/Users/adrian/Desktop"; // Adjust this to a valid path
+        let permission_input = "InvalidPermission";
+
+        let permission = Permission::from_str(permission_input);
+        assert!(permission.is_err());
+    }
+
+    // Test for insufficient permission with read permission
+    #[test]
+    fn test_insufficient_permission_read() {
+        let user = "valid_user";
+        let file_path = "/Users/adrian/Desktop"; // This should match the exact string in your function's logic
+        let permission = Permission::Read;
+    
+        let result = verify_permissions(user, file_path, permission);
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err().kind(), io::ErrorKind::PermissionDenied);
+    }
+
+    // Test for insufficient permission with execute permission
+    #[test]
+    fn test_valid_user_valid_path_execute_permission() {
+        let user = "valid_user";
+        let file_path = "/Users/adrian/Desktop";
+        let permission = Permission::Execute;
+        let result = verify_permissions(user, file_path, permission);
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err().kind(), io::ErrorKind::PermissionDenied);
+    }
+
+    // Test for a valid user, a valid file path, and the write permission
+    #[test]
+    fn test_valid_user_valid_path_write_permission() {
+        let user = "valid_user";
+        let file_path = "/Users/adrian/Desktop"; // Adjust this to a valid path
+        let permission = Permission::Write;
+
+        assert!(matches!(verify_permissions(user, file_path, permission), Ok(())));
+    }
+}
