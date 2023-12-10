7fc77ab87785c5d59be82aa6e968d4920cae7001
andyyuyc <76409596+andyyuyc@users.noreply.github.com>
2023-12-09 19:32:44 -0500

    main + log

---
+++ src/log.rs
@@ -0,0 +1,50 @@
+use std::fs::{OpenOptions, create_dir_all};
+use std::io::Write;
+use std::path::{Path, PathBuf};
+use std::fs::File;
+use std::io::{self, Read};
+
+use crate::file_management::commit::Commit; 
+
+// Write the file
+// This function takes a `Commit` object and appends its information to the log file.
+pub fn dvcs_log(commit: &Commit) -> std::io::Result<()> {
+    // Make sure the `.my-dvcs/logs` directory exists.
+    let dvcs_dir = Path::new(".my-dvcs");
+    let logs_dir = dvcs_dir.join("logs");
+    create_dir_all(&logs_dir)?;
+
+    // Open the log file and create it if it doesn't exist
+    let log_file_path = logs_dir.join("log.txt");
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open(log_file_path)?;
+
+    // Write the commit details to a file
+    writeln!(file, "{}", commit)?;
+
+    Ok(())
+}
+
+
+// Read the file
+pub fn log() -> io::Result<()> {
+    // Define the path to the log file
+    let dvcs_dir = Path::new(".my-dvcs");
+    let logs_dir = dvcs_dir.join("logs");
+    let log_file_path = logs_dir.join("log.txt");
+
+    // Open the log file
+    let mut file = File::open(log_file_path)?;
+
+    // Read the contents of the file
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    // Print the contents to the console
+    println!("{}", contents);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 // main.rs
-use tokio::runtime;
+
+
 mod init;
 mod clone;
 mod status;
 mod errorhandling;
+mod log;
+mod file_management;
+
+use crate::{file_management::{commit::commit, hash::DVCSHash}, state_management::checkout::checkout};
+use crate::file_management::commit; 
+use crate::file_management::commit::Commit; 
+
+mod interface;
+mod state_management;
 
 #[tokio::main]
 async fn main() {
@@ -18,11 +28,18 @@ async fn main() {
         "init" => init::init(),
         "clone" => clone::clone(),
         "errorhandling" => errorhandling::errorhandling(),
+        "log" => {
+            if let Err(e) = log::log() {
+                println!("Error logging commit: {}", e);
+            }
+        },
+        
         "status" => {
             if let Err(e) = status::status().await {
                 println!("Error: {}", e);
             }
         },
+        
         _ => println!("Unknown command"),
         
     }
