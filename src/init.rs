eb984dab921b3ae4dddb0135ef7bb46ddfed0c39
andyyuyc <76409596+andyyuyc@users.noreply.github.com>
2023-12-09 20:42:47 -0500

    init

--- src/init.rs
+++ src/init.rs
@@ -1,3 +1,4 @@
+
 use git2::{Repository, Error};
 use std::io::{self, Write};
 use std::fs::{self, DirEntry};
@@ -5,54 +6,64 @@ use std::path::Path;
 use std::io::Result as IoResult;
 use std::process::Command;
 use std::fs::DirBuilder;
+use crate::interface::io::RepositoryInterface;
+
+
 
 fn init_repo(path: &str) -> IoResult<()> {
+    let repo_path = Path::new(path);
+
+    // Check if '.git' directory exists in the provided path
+    if repo_path.join(".git").exists() {
+        return Err(io::Error::new(io::ErrorKind::AlreadyExists, "Git repository already exists in the provided path"));
+    }
+
+    // Proceed with the repository initialization
     let status = Command::new("git")
         .arg("init")
         .arg(path)
         .status()?;
     
-    let repo_path = Path::new(path);
-    
-    // 创建主目录 .my-dvcs
+
+
+    // Create the home directory .my-dvcs
     let dvcs_path = repo_path.join(".my-dvcs");
     DirBuilder::new().recursive(true).create(&dvcs_path)?;
 
-    // 创建 obj, branches 和 heads 子目录
+    // Create the obj, branches and heads subdirectories.
     for subdir in ["obj", "branches"].iter() {
         DirBuilder::new().recursive(true).create(dvcs_path.join(subdir))?;
     }
 
-    // 在 branches 目录下创建一个 head 文件，并写入默认分支的名称
-    let branches_path = dvcs_path.join("branches");
-    let default_branch = ""; // 这里设置默认分支的名称
-    let head_path = branches_path.join("HEAD");
+    // Create a head file in the branches directory with the name of the default branch
+    let default_branch = ""; // Set the name of the default branch here
+    let head_path = dvcs_path.join("head");
     fs::write(head_path, default_branch)?;
 
     if status.success() {
         Ok(())
     } else {
-        Err(io::Error::new(io::ErrorKind::Other, "Failed to initialize git repository"))
+        Err(io::Error::new(io::ErrorKind::Other, "Failed to initialize repository"))
     }
 }
 
 
 
 
-/// 从用户获取输入
+/// Getting input from the user
 fn get_input(prompt: &str) -> String {
     let mut input = String::new();
     print!("{}", prompt);
-    io::stdout().flush().unwrap(); // 确保立即显示提示
+    io::stdout().flush().unwrap(); // Ensure prompts are displayed immediately
     io::stdin().read_line(&mut input).expect("Failed to read line");
     input.trim().to_owned()
 }
 
 pub fn init() { 
-    // 获取用户输入的仓库路径
+    // Get the path to the repository as entered by the user
     let repo_path = get_input("Enter the path to initialize a new repository: ");
 
-    // 尝试初始化仓库
+    // Try to initialize the repository
     match init_repo(&repo_path) {
         Ok(_) => println!("Repository initialized at {}", repo_path),
         Err(e) => println!("Failed to initialize repository: {}", e),
